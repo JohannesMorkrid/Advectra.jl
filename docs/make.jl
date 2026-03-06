507d09d228ff9b7cfb751e2359d9703aa8092ad6
JohannesMorkrid <jomor9370@uit.no>
2026-03-06 15:14:51 +0100

    Updated docs make file

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,10 @@
-using Advectra, Documenter
-DocMeta.setdocmeta!(Advectra, :DocTestSetup, :(using Advectra);
-                    recursive=true)
+using Documenter
+using Advectra
+DocMeta.setdocmeta!(Advectra, :DocTestSetup, :(using Advectra); recursive=true)
 
 makedocs(; sitename="Advectra",
          authors="Johannes Mørkrid",
          modules=[Advectra],
          warnonly=[:doctest, :missing_docs])
 
-deploydocs(; repo="github.com/JohannesMorkrid/Advectra.jl.git")
+deploydocs(; repo="https://github.com/JohannesMorkrid/Advectra.jl")
