dfdee9dbb148a59c84872bde6d842990e4519f7d
JohannesMorkrid <jomor9370@uit.no>
2026-03-31 16:22:55 +0200

    Attempt to force build

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,10 @@ DocMeta.setdocmeta!(Advectra, :DocTestSetup, :(using Advectra); recursive=true)
 makedocs(; sitename="Advectra",
          authors="Johannes Mørkrid",
          modules=[Advectra],
-         warnonly=[:doctest, :missing_docs])
+         warnonly=[:doctest,
+             :missing_docs,
+             :docs_block,
+             :autodocs_block,
+             :cross_references])
 
 deploydocs(; repo="github.com/JohannesMorkrid/Advectra.jl")
