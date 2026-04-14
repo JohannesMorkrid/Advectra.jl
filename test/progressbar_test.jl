72f659c68be7a406d452921c6c689a37237432ab
Johannes Mørkrid <43619041+JohannesMorkrid@users.noreply.github.com>
2026-04-14 11:19:34 +0200

    Refactor progressbar test variable names and logic
    Removed unused test_range, and renamed variables to mirror whats the "norm"

--- test/progressbar_test.jl
+++ test/progressbar_test.jl
@@ -4,19 +4,18 @@
 using Advectra
 
 @testset "Diagnostics and Progress" begin
-    nx, ny = 256, 256
-    domain = Domain(nx, ny)
+    Nx, Ny = 256, 256
+    domain = Domain(Nx, Ny)
     prob = (; domain=domain)
     tspan = (0.0, 1.0) # Use a tuple for standard tspan convention
     dt = 1e-1
-    test_range = 0.0:dt:0.05
 
     ic = initial_condition(isolated_blob, domain)
     ic_hat = spectral_transform(ic, get_fwd(domain))
 
     progress = build_diagnostic(Val(:progress); tspan=tspan, dt=dt)
     for i in 0.0:dt:1.0
-        progress(ic_hat, prob, i)
+        progress(ic_hat, prob, i*dt)
         sleep(0.005)
     end
 
