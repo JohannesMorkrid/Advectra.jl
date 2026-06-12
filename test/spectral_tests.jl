a8a81ab9b16cb2810eaed8380dfb8bd81d7d446f
JohannesMorkrid <jomor9370@uit.no>
2026-06-12 14:08:35 +0200

    Included some test debug script, that can later be turned into a integration test to make sure spectral_sum works as expected.

--- test/spectral_tests.jl
+++ test/spectral_tests.jl
@@ -64,3 +64,92 @@ for state with multiple fields.
 :poloidal and :wavenumber
 * Test that other spectrum symbols throws an error
 """
+
+using Advectra
+rdomain = Domain(257; L=48, real_transform=true)
+domain = Domain(257; L=48, real_transform=false)
+
+A = randn(257, 257)
+Ar_hat = get_fwd(rdomain)*A
+A_hat = get_fwd(domain)*A
+f = abs2 #identity
+dims = 2
+
+using Statistics
+A_zonal = mean(A; dims=1)
+A_streamer = mean(A; dims=2)
+
+A_zonal_hat = selectdim(A_hat, 1, 1:1)
+A_zonal_hat == selectdim(Ar_hat, 1, 1:1)
+
+A_streamer_hat = selectdim(A_hat, 2, 1:1)
+A_streamer_hat == selectdim(Ar_hat, 2, 1:1)
+
+# Using normal transform
+
+# Build operators
+diff_x = build_operator(:diff_x, domain)
+diff_y = build_operator(:diff_y, domain)
+
+A_zonal = mean(A; dims=1) .+ 0.0*domain.y
+A_streamer = mean(A; dims=2) .+ 0.0*domain.x'
+
+A_zonal_hat = get_fwd(domain)*A_zonal
+A_streamer_hat = get_fwd(domain)*A_streamer
+
+#A_zonal_hat = selectdim(A_hat, 1, 1:1)
+#A_streamer_hat = selectdim(A_hat, 2, 1:1)
+
+vx_hat = -diff_y(A_streamer_hat)
+vy_hat = diff_x(A_zonal_hat)
+
+vx = get_bwd(domain)*vx_hat
+vy = get_bwd(domain)*vy_hat
+
+mean(abs2, vx)
+mean(abs2, vy)
+
+A_hat = get_fwd(domain)*A
+A_zonal_hat = selectdim(A_hat, 1, 1:1)
+A_streamer_hat = selectdim(A_hat, 2, 1:1)
+
+vx_hat = -diff_y(A_streamer_hat)
+vy_hat = diff_x(A_zonal_hat)
+
+spectral_sum(abs2, vx_hat, domain)/length(domain)^2 ≈ mean(abs2, vx)
+spectral_sum(abs2, vy_hat, domain)/length(domain)^2 ≈ mean(abs2, vy)
+
+# Using real transform
+
+rdiff_x = build_operator(:diff_x, rdomain)
+rdiff_y = build_operator(:diff_y, rdomain)
+
+#Ar_zonal = mean(A; dims=1) .+ 0.0*rdomain.y
+#Ar_streamer = mean(A; dims=2) .+ 0.0*rdomain.x'
+
+#Ar_zonal_hat = get_fwd(rdomain)*Ar_zonal
+#Ar_streamer_hat = get_fwd(rdomain)*Ar_streamer
+
+Ar_hat = get_fwd(rdomain)*A
+Ar_zonal_hat = selectdim(Ar_hat, 1, 1:1)
+Ar_streamer_hat = selectdim(Ar_hat, 2, 1:1)
+
+rvx_hat = -rdiff_y(Ar_streamer_hat)
+rvy_hat = rdiff_x(Ar_zonal_hat)
+
+spectral_sum(abs2, rvx_hat, rdomain)/length(domain)^2 ≈ mean(abs2, vx)
+spectral_sum(abs2, rvy_hat, rdomain)/length(domain)^2 ≈ mean(abs2, vy)
+
+# Tests
+spectral_sum(f, A, domain)
+spectral_sum(spectral_sum(f, A, domain; dims=1), domain)
+spectral_sum(spectral_sum(f, A, domain; dims=2), domain)
+spectral_sum(spectral_sum(f, A, domain; dims=2), domain; dims=1)[1]
+spectral_sum(spectral_sum(f, A, domain; dims=1), domain; dims=2)[1]
+spectral_sum(spectral_sum(f, A, domain; dims=1), domain; dims=1)
+spectral_sum(spectral_sum(f, A, domain; dims=2), domain; dims=2)
+
+spectral_sum(f, selectdim(A, 1, 1), domain)
+spectral_sum(f, selectdim(A, 1, 1:1), domain)
+spectral_sum(f, selectdim(A, 2, 1:1), domain)
+spectral_sum(f, selectdim(A, 2, 1), domain)
